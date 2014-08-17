6f35039301990eafe697fdfdcca601e690e45d8f
kiwiaceofspades <alistair.eichler.ask@gmail.com>
2014-08-17 16:33:37 +1200

    Created acusation classs Enabled game to load cards and make the correct accusation

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,20 +1,20 @@
 package main;
 
-import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.image.ImageObserver;
-import java.awt.image.ImageProducer;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
-import sun.awt.image.ImageFormatException;
+import GamePieces.Acusation;
 import GamePieces.Card;
 import GamePieces.Person;
 
 public class Game {
 	
-	List<Person> players;
-	List<Card> cards;
+	private List<Person> players;
+	private List<Card> cards;
+	private Acusation murder;
+	
 	
 	
 	public Game(){
@@ -23,12 +23,48 @@ public class Game {
 	}
 	
 	public void loadCards(){
-		cards = new ArrayList<Card>();
-		cards.add(new Card("Candlestick", "assets/weapons/Candlestick-1949.png", Card.Type.WEAPON));
-		cards.add(new Card("Knife","assets/weapons/Knife-1949.png", Card.Type.WEAPON));
-		cards.add(new Card("Lead Pipe","assets/weapons/Lead-Pipe-1949.png", Card.Type.WEAPON));
-		cards.add(new Card("Revolver","assets/weapons/Revoler-1949.png", Card.Type.WEAPON));
-		cards.add(new Card("Rope","assets/weapons/Rope-1949.png", Card.Type.WEAPON));
-		cards.add(new Card("Wrench","assets/weapons/Wrench-1949.png", Card.Type.WEAPON));
+		//load weapons and select index for murder weapon
+		ArrayList<Card> weapons = new ArrayList<Card>();
+		weapons.add(new Card("Candlestick", "assets/weapons/Candlestick-1949.png", Card.Type.WEAPON));
+		weapons.add(new Card("Knife","assets/weapons/Knife-1949.png", Card.Type.WEAPON));
+		weapons.add(new Card("Lead Pipe","assets/weapons/Lead-Pipe-1949.png", Card.Type.WEAPON));
+		weapons.add(new Card("Revolver","assets/weapons/Revoler-1949.png", Card.Type.WEAPON));
+		weapons.add(new Card("Rope","assets/weapons/Rope-1949.png", Card.Type.WEAPON));
+		weapons.add(new Card("Wrench","assets/weapons/Wrench-1949.png", Card.Type.WEAPON));
+		int accWep = new Random().nextInt(weapons.size());
+		
+		//load rooms and select an index for murder location
+		ArrayList<Card> rooms = new ArrayList<Card>();
+		rooms.add(new Card("Ballroom","Ballroom.jpg",Card.Type.ROOM));
+		rooms.add(new Card("Billiard Room","Billiard-room.jpg",Card.Type.ROOM));
+		rooms.add(new Card("Conservatory","Conservatory.jpg",Card.Type.ROOM));
+		rooms.add(new Card("Dining Room","Dining-room.jpg",Card.Type.ROOM));
+		rooms.add(new Card("Hall","Hall.jpg",Card.Type.ROOM));
+		rooms.add(new Card("Kitchen","Kitchen.jpg",Card.Type.ROOM));
+		rooms.add(new Card("Library","Library.jpg",Card.Type.ROOM));
+		rooms.add(new Card("Lounge","Lounge.jpg",Card.Type.ROOM));
+		rooms.add(new Card("Study","Study.jpg",Card.Type.ROOM));
+		int accRoom = new Random().nextInt(rooms.size());
+		
+		//load people and select an index for murderer
+		ArrayList<Card> people = new ArrayList<Card>();
+		people.add(new Card("Colonel Mustard","Colonel-Mustard.jpg",Card.Type.PERSON));
+		people.add(new Card("Miss Scarlet","Miss-Scarlet.jpg",Card.Type.PERSON));
+		people.add(new Card("Mr Green","Mr-Green.jpg",Card.Type.PERSON));
+		people.add(new Card("Mrs Peacock","Mrs-Peacock.jpg",Card.Type.PERSON));
+		people.add(new Card("Mrs White","Mrs-White.jpg",Card.Type.PERSON));
+		people.add(new Card("Prof Plum","Prof-Plum.jpg",Card.Type.PERSON));
+		int accPer = new Random().nextInt(rooms.size());
+		
+		//creates the correct acusation & removes those cards from the list
+		murder = new Acusation(weapons.get(accWep), people.get(accPer), rooms.get(accRoom));
+		weapons.remove(accWep);
+		people.remove(accPer);
+		rooms.remove(accWep);
+		
+		//finally merges the three lists to a single list
+		cards.addAll(weapons);
+		cards.addAll(rooms);
+		cards.addAll(people);
 	}
 }
